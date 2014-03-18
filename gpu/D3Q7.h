556c06738ac8afdd9222f205fb544f8d2274f955
James E McClure <jemcclur@gmail.com>
2014-03-18 11:56:36 -0400

    refactored D3Q7.cu /D3Q7.h to match CPU calls

--- gpu/D3Q7.h
+++ gpu/D3Q7.h
@@ -1,16 +1,16 @@
 // CPU Functions for D3Q7 Lattice Boltzmann Methods
 
-extern "C" void dvc_PackValues(int *list, int count, double *sendbuf, double *Data, int N);
+extern "C" void PackValues(int *list, int count, double *sendbuf, double *Data, int N);
 
-extern "C" void dvc_UnpackValues(int *list, int count, double *recvbuf, double *Data, int N);
+extern "C" void UnpackValues(int *list, int count, double *recvbuf, double *Data, int N);
 
-extern "C" void dvc_PackDenD3Q7(int *list, int count, double *sendbuf, int number, double *Data, int N);
+extern "C" void PackDenD3Q7(int *list, int count, double *sendbuf, int number, double *Data, int N);
 
-extern "C" void dvc_UnpackDenD3Q7(int *list, int count, double *recvbuf, int number, double *Data, int N);
+extern "C" void UnpackDenD3Q7(int *list, int count, double *recvbuf, int number, double *Data, int N);
 
-extern "C" void dvc_InitD3Q7(char *ID, double *f_even, double *f_odd, double *Den, int Nx, int Ny, int Nz);
+extern "C" void InitD3Q7(char *ID, double *f_even, double *f_odd, double *Den, int Nx, int Ny, int Nz);
 
-extern "C" void dvc_SwapD3Q7(char *ID, double *disteven, double *distodd, int Nx, int Ny, int Nz);
+extern "C" void SwapD3Q7(char *ID, double *disteven, double *distodd, int Nx, int Ny, int Nz);
 
-extern "C" void dvc_ComputeDensityD3Q7(char *ID, double *disteven, double *distodd, double *Den, 
+extern "C" void ComputeDensityD3Q7(char *ID, double *disteven, double *distodd, double *Den,
 										int Nx, int Ny, int Nz);
