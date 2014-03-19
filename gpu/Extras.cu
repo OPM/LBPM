7f68548125574d1d2e49926ad47153416c9958d8
James E McClure <jemcclur@gmail.com>
2014-03-19 09:07:50 -0400

    Refactored Extras.cu / Extras.h for gpu build

--- gpu/Extras.cu
+++ gpu/Extras.cu
@@ -1,19 +1,19 @@
 // Basic cuda functions callable from C/C++ code
 #include <cuda.h>
 
-extern "C" void dvc_AllocateDeviceMemory(void** address, size_t size){
+extern "C" void AllocateDeviceMemory(void** address, size_t size){
 	cudaMalloc(address,size);
 }
 
-extern "C" void dvc_CopyToDevice(void* dest, void* source, size_t size){
+extern "C" void CopyToDevice(void* dest, void* source, size_t size){
 	cudaMemcpy(dest,source,size,cudaMemcpyHostToDevice);
 }
 
 
-extern "C" void dvc_CopyToHost(void* dest, void* source, size_t size){
+extern "C" void CopyToHost(void* dest, void* source, size_t size){
 	cudaMemcpy(dest,source,size,cudaMemcpyDeviceToHost);
 }
 
-extern "C" void dvc_Barrier(){
+extern "C" void DeviceBarrier(){
 	cudaDeviceSynchronize();
 }
