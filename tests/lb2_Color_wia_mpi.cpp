0ce3d0f045e6b1f41869528f9afd64cca698d7bc
James McClure <mcclurej@vt.edu>
2014-04-21 14:08:57 -0400

    set trimdist=0 as a test

--- tests/lb2_Color_wia_mpi.cpp
+++ tests/lb2_Color_wia_mpi.cpp
@@ -1065,7 +1065,7 @@ int main(int argc, char **argv)
 	// Averaging variables
 	//...........................................................................
 	// local averages (to each MPI process)
-	double trimdist=3.0; 						// pixel distance to trim surface for specified averages
+	double trimdist=0.0; 						// pixel distance to trim surface for specified averages
 	double awn,ans,aws,lwns,nwp_volume;
 	double As;
 	double vol_w, vol_n;						// volumes the exclude the interfacial region
