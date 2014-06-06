97e34a6cb8ac7185379ba39358309a5ba54f7a5c
James McClure <mcclurej@vt.edu>
2014-06-06 12:34:45 -0400

    Mapped trRwn to radius in tests/lb2_Color_wia_mpi.cpp

--- tests/lb2_Color_wia_mpi.cpp
+++ tests/lb2_Color_wia_mpi.cpp
@@ -2348,6 +2348,8 @@ int main(int argc, char **argv)
 			
 			if (trawn_global > 0.0)	trJwn_global /= trawn_global;
 			if (trawn_global > 0.0)	trRwn_global /= trawn_global;
+			trRwn_global *= 2.0;
+			
 
 			if (awn_global > 0.0)	for (i=0; i<3; i++)		vawn_global(i) /= awn_global;
 			if (awn_global > 0.0)	for (i=0; i<6; i++)		Gwn_global(i) /= awn_global;
