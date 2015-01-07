e7fe316adc9b29f6a224bcd11a8aa579b143dfb1
James E McClure <jemcclur@gmail.com>
2015-01-07 13:43:00 -0500

    Added a unit test for TwoPhase averaging class tests/TestTwoPhase.cpp

---
+++ tests/TestTwoPhase.cpp
@@ -0,0 +1,26 @@
+// Unit test for TwoPhase averaging class
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <exception>
+#include <stdexcept>
+#include <fstream>
+
+#include "pmmc.h"
+#include "Domain.h"
+#include "Extras.h"
+#include "D3Q19.h"
+#include "D3Q7.h"
+#include "Color.h"
+#include "common/MPI.h"
+#include "Communication.h"
+#include "IO/Mesh.h"
+#include "IO/Writer.h"
+#include "ProfilerApp.h"
+
+int main(int argc, char **argv)
+{
+
+	return 0;
+}
