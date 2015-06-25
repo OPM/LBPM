1c2b3991ec3ff63b688a1e31c71ebc82933da5bb
James E McClure <mcclurej@vt.edu>
2015-06-25 16:18:45 -0400

    Adding error handling to gpu/Extras.cu

--- gpu/Extras.cu
+++ gpu/Extras.cu
@@ -2,16 +2,28 @@
 #include <cuda.h>
 
 extern "C" void AllocateDeviceMemory(void** address, size_t size){
-	cudaMalloc(address,size);
+       	cudaMalloc(address,size);
+	cudaError_t err = cudaGetLastError();
+	if (cudaSuccess != err){
+	   printf("Error in cudaMalloc\n");
+	}	
 }
 
 extern "C" void CopyToDevice(void* dest, void* source, size_t size){
 	cudaMemcpy(dest,source,size,cudaMemcpyHostToDevice);
+	cudaError_t err = cudaGetLastError();
+	if (cudaSuccess != err){
+	   printf("Error in cudaMemcpy host to device \n");
+	}
 }
 
 
 extern "C" void CopyToHost(void* dest, void* source, size_t size){
 	cudaMemcpy(dest,source,size,cudaMemcpyDeviceToHost);
+	cudaError_t err = cudaGetLastError();
+	if (cudaSuccess != err){
+	   printf("Error in cudaMemcpy device to host \n");
+	}
 }
 
 extern "C" void DeviceBarrier(){
