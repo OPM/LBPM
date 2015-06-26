86d9f6b604b2595430f1cfa5035b54ac87adcb77
James E McClure <mcclurej@vt.edu>
2015-06-26 15:42:59 -0400

    dbg

--- common/TwoPhase.h
+++ common/TwoPhase.h
@@ -469,7 +469,7 @@ void TwoPhase::ComputeLocal(){
 		// Compute volume averages
 		for (int p=0;p<8;p++){
 			int np_cube = i+cube[p][0] + (j+cube[p][1])*Nx + (k+cube[p][2])*Nx*Ny;
-			if ( Dm.id[n] != 0 ){
+			if ( Dm.id[np_cube] != 0 ){
 				// 1-D index for this cube corner
 				n = i+cube[p][0] + (j+cube[p][1])*Nx + (k+cube[p][2])*Nx*Ny;
 				// compute the norm of the gradient of the phase indicator field
@@ -613,7 +613,7 @@ void TwoPhase::ComputeLocalBlob(){
 		// Compute volume averages
 		for (int p=0;p<8;p++){
 			int np_cube = i+cube[p][0] + (j+cube[p][1])*Nx + (k+cube[p][2])*Nx*Ny;
-			if ( Dm.id[n] != 0 ){
+			if ( Dm.id[np_cube] != 0 ){
 				// 1-D index for this cube corner
 				n = i+cube[p][0] + (j+cube[p][1])*Nx + (k+cube[p][2])*Nx*Ny;
 				// compute the norm of the gradient of the phase indicator field
