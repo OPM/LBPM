75b1e35564db240949b10ea0d39a143a77cad704
James E McClure <mcclurej@vt.edu>
2015-06-26 14:57:25 -0400

    Updating timestep for averaging in lbpm_color_simuator, should be a nice stable version :)

--- tests/lbpm_color_simulator.cpp
+++ tests/lbpm_color_simulator.cpp
@@ -1561,7 +1561,7 @@ int main(int argc, char **argv)
 		// Timestep completed!
 		timestep++;
 		//...................................................................
-		if (timestep%100 == 95){
+		if (timestep%5000 == 4995){
 			//...........................................................................
 			// Copy the phase indicator field for the earlier timestep
 			DeviceBarrier();
@@ -1569,7 +1569,7 @@ int main(int argc, char **argv)
 	//		Averages.ColorToSignedDistance(beta,Averages.Phase,Averages.Phase_tplus);
 			//...........................................................................
 		}
-		if (timestep%100 == 0){
+		if (timestep%5000 == 0){
 			//...........................................................................
 			// Copy the data for for the analysis timestep
 			//...........................................................................
@@ -1584,7 +1584,7 @@ int main(int argc, char **argv)
 			CopyToHost(Averages.Vel_z.get(),&Velocity[2*N],N*sizeof(double));
 			MPI_Barrier(MPI_COMM_WORLD);
 		}
-		if (timestep%100 == 5){
+		if (timestep%5000 == 5){
 			//...........................................................................
 			// Copy the phase indicator field for the later timestep
 			DeviceBarrier();
