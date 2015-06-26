ee182c32595690587d9a2c99e8d8dfb505540ee4
James E McClure <mcclurej@vt.edu>
2015-06-26 18:44:27 -0400

    bug has resurfaced oh no

--- tests/lbpm_color_simulator.cpp
+++ tests/lbpm_color_simulator.cpp
@@ -1558,6 +1558,7 @@ int main(int argc, char **argv)
 
 		MPI_Barrier(MPI_COMM_WORLD);
 
+		//
 		// Timestep completed!
 		timestep++;
 		//...................................................................
