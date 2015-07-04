465853410be13a0edcecd800d43a5b9accefa7f5
James E McClure <mcclurej@vt.edu>
2015-07-04 13:55:40 -0400

    Fixed bug in gpu/Color.cu

--- gpu/Color.cu
+++ gpu/Color.cu
@@ -1181,7 +1181,7 @@ __global__  void dvc_MassColorCollideD3Q7(char *ID, double *A_even, double *A_od
 			a1 = na*(0.1111111111111111*(1+4.5*uz))+delta;
 			b1 = nb*(0.1111111111111111*(1+4.5*uz))-delta;
 			a2 = na*(0.1111111111111111*(1-4.5*uz))-delta;
-			b2 = nb*(0.1111111111111111*(1-4.5*uz)+delta);
+			b2 = nb*(0.1111111111111111*(1-4.5*uz))+delta;
 			
 			A_odd[2*N+n] = a1;
 			A_even[3*N+n] = a2;
