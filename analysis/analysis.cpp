e61fab7effbbc93182b7062bd0c8bf9fae5b21e8
James E McClure <mcclurej@vt.edu>
2015-07-13 13:47:38 -0400

    debugging

--- analysis/analysis.cpp
+++ analysis/analysis.cpp
@@ -632,6 +632,7 @@ int ComputeGlobalPhaseComponent( int nx, int ny, int nz, RankInfoStruct rank_inf
     printf("fillData %ix%ix%i \n",nx,ny,nz);
     fillHalo<int> fillData(rank_info,nx,ny,nz,1,1,1,0,1,true,true,true);
     fillData.fill(GlobalBlobID);
+    printf("Failure here \n");
     // Create a list of all neighbor ranks (excluding self)
     std::vector<int> neighbors;
     neighbors.push_back( rank_info.rank[0][1][1] );
