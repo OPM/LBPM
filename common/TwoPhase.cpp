e382b33d7774387dfa3b0a1db4d8138e3d5f3117
James E McClure <mcclurej@vt.edu>
2015-09-01 21:20:22 -0400

    fixed bug

--- common/TwoPhase.cpp
+++ common/TwoPhase.cpp
@@ -728,7 +728,7 @@ void TwoPhase::ComponentAverages()
 	MPI_Barrier(Dm.Comm);
 //	MPI_Allreduce(&ComponentAverages_NWP(0,0),&RecvBuffer(0,0),BLOB_AVG_COUNT*NumberComponents_NWP,
 //					MPI_DOUBLE,MPI_SUM,MPI_COMM_WORLD);
-	MPI_Reduce(&ComponentAverages_NWP(0,b),&RecvBuffer(0),BLOB_AVG_COUNT,MPI_DOUBLE,MPI_SUM,0,Dm.Comm);
+	MPI_Reduce(&ComponentAverages_NWP(0,0),&RecvBuffer(0),BLOB_AVG_COUNT,MPI_DOUBLE,MPI_SUM,0,Dm.Comm);
 
 	for (int b=0; b<NumberComponents_NWP; b++){
 		for (int idx=0; idx<BLOB_AVG_COUNT; idx++) ComponentAverages_NWP(idx,b)=RecvBuffer(idx,b);
