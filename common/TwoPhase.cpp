3b7c87281cf6f41b0b740715c34620f7d9c1cfcd
James E McClure <mcclurej@vt.edu>
2015-11-12 11:10:06 -0500

    Returning SSO to ColortoSignDist

--- common/TwoPhase.cpp
+++ common/TwoPhase.cpp
@@ -179,7 +179,7 @@ void TwoPhase::ColorToSignedDistance(double Beta, DoubleArray &ColorData, Double
 {
 	double factor,temp,value;
 	factor=0.5/Beta;
-/*	// Initialize to -1,1 (segmentation)
+	// Initialize to -1,1 (segmentation)
 	for (int k=0; k<Nz; k++){
 		for (int j=0; j<Ny; j++){
 			for (int i=0; i<Nx; i++){
@@ -212,7 +212,7 @@ void TwoPhase::ColorToSignedDistance(double Beta, DoubleArray &ColorData, Double
 	    }
 	  }
 	}	
-        */
+        /*
 		for (int k=0; k<Nz; k++){
 		for (int j=0; j<Ny; j++){
 			for (int i=0; i<Nx; i++){
@@ -220,7 +220,7 @@ void TwoPhase::ColorToSignedDistance(double Beta, DoubleArray &ColorData, Double
 			}
 		}
 	}
-
+*/
 //	for (int n=0; n<Nx*Ny*Nz; n++)	DistData[n] = ColorData[n];
 }
 
