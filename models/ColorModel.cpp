9d60c0f949bf29251f35772118eb22e0ed262eb5
James E McClure <jemcclur@gmail.com>
2019-03-26 11:21:25 -0400

    fix print bug

--- models/ColorModel.cpp
+++ models/ColorModel.cpp
@@ -784,7 +784,7 @@ double ScaLBL_ColorModel::MorphInit(const double beta, const double target_delta
 
 	if (volume_connected < 0.02*volume_initial){
 		// if connected volume is less than 2% just delete the whole thing
-		if (rank==0) printf("Connected region has shrunk to less than 2% of total fluid volume \n");
+		if (rank==0) printf("Connected region has shrunk to less than 2 %% of total fluid volume \n");
 		REVERSE_FLOW_DIRECTION = true;
 	}
 
