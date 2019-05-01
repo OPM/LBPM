28b1e580c2731bda70df688bff0ca5cf110ac6ae
JamesEMcclure <jemcclur@gmail.com>
2019-05-01 16:47:39 -0400

    fix IO bug in subphase

--- analysis/SubPhase.cpp
+++ analysis/SubPhase.cpp
@@ -93,7 +93,7 @@ SubPhase::SubPhase(std::shared_ptr <Domain> dm):
 		{
 			// If timelog is empty, write a short header to list the averages
 			//fprintf(TIMELOG,"--------------------------------------------------------------------------------------\n");
-			fprintf(TIMELOG,"sw krw krn qw qn pw pn force\n");				
+			fprintf(TIMELOG,"sw krw krn qw qn pw pn\n");				
 		}
 	}
 }
@@ -254,7 +254,7 @@ void SubPhase::Basic(){
 		double krn = h*h*nu_n*not_water_flow_rate / force_mag ;
 		double krw = h*h*nu_w*water_flow_rate / force_mag;
 		//printf("   water saturation = %f, fractional flow =%f \n",saturation,fractional_flow);
-		fprintf(TIMELOG,"%.5g %.5g %.5g %.5g %.5g %.5g %.5g\n",saturation,krw,krn,h*h*h*water_flow_rate,h*h*h*not_water_flow_rate, gwb.p, gnb.p,force_mag); 
+		fprintf(TIMELOG,"%.5g %.5g %.5g %.5g %.5g %.5g %.5g\n",saturation,krw,krn,h*h*h*water_flow_rate,h*h*h*not_water_flow_rate, gwb.p, gnb.p); 
 		fflush(TIMELOG);
 	}
 
