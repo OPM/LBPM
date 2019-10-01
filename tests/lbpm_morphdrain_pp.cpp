323dd7d24b6c49c5593fc171c5e40ec939370019
James E McClure <jemcclur@gmail.com>
2019-09-30 22:39:01 -0400

    fix bug

--- tests/lbpm_morphdrain_pp.cpp
+++ tests/lbpm_morphdrain_pp.cpp
@@ -205,7 +205,7 @@ int main(int argc, char **argv)
 				}
 			}
 		}
-		sprintf(FILENAME,READFILE,"morphdrain.raw");
+		sprintf(FILENAME,READFILE.c_str(),"morphdrain.raw");
 		Mask->AggregateLabels(FILENAME);
 
 	}
