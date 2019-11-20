4878557e20c87680d002d23016a0c8f469c1fe3c
JamesEMcclure <jemcclur@gmail.com>
2019-11-20 09:53:44 -0500

    update labels in loop

--- common/ReadMicroCT.cpp
+++ common/ReadMicroCT.cpp
@@ -83,38 +83,38 @@ Array<uint8_t> readMicroCT( const Database& domain, MPI_Comm comm )
     Array<uint8_t> data;
     RankInfoStruct srcRankInfo( rank, Nfx, Nfy, Nfz );
     if ( srcRankInfo.ix >= 0 ) {
-        auto filename = domain.getScalar<std::string>( "Filename" );
-        char tmp[100];
-        if ( filename.find( "0x_0y_0z.gbd.gz" ) != std::string::npos ) {
-            sprintf( tmp, "%ix_%iy_%iz.gbd.gz", srcRankInfo.ix, srcRankInfo.jy, srcRankInfo.kz );
-            filename = filename.replace( filename.find( "0x_0y_0z.gbd.gz" ), 15, std::string( tmp ) );
-        } else if ( filename.find( "x0_y0_z0.gbd.gz" ) != std::string::npos ) {
-            sprintf( tmp, "x%i_y%i_z%i.gbd.gz", srcRankInfo.ix, srcRankInfo.jy, srcRankInfo.kz );
-            filename = filename.replace( filename.find( "x0_y0_z0.gbd.gz" ), 15, std::string( tmp ) );
-        } else {
-            ERROR( "Invalid name for first file" );
-        }
-        data = readMicroCT( filename );
+    	auto filename = domain.getScalar<std::string>( "Filename" );
+    	char tmp[100];
+    	if ( filename.find( "0x_0y_0z.gbd.gz" ) != std::string::npos ) {
+    		sprintf( tmp, "%ix_%iy_%iz.gbd.gz", srcRankInfo.ix, srcRankInfo.jy, srcRankInfo.kz );
+    		filename = filename.replace( filename.find( "0x_0y_0z.gbd.gz" ), 15, std::string( tmp ) );
+    	} else if ( filename.find( "x0_y0_z0.gbd.gz" ) != std::string::npos ) {
+    		sprintf( tmp, "x%i_y%i_z%i.gbd.gz", srcRankInfo.ix, srcRankInfo.jy, srcRankInfo.kz );
+    		filename = filename.replace( filename.find( "x0_y0_z0.gbd.gz" ), 15, std::string( tmp ) );
+    	} else {
+    		ERROR( "Invalid name for first file" );
+    	}
+    	data = readMicroCT( filename );
+
+    	// Relabel the data
+    	for (int k = 0; k<1024; k++){
+    		for (int j = 0; j<1024; j++){
+    			for (int i = 0; i<1024; i++){
+    				//n = k*Nfx*Nfy + j*Nfx + i;
+    				//char locval = loc_id[n];
+    				char locval = data(i,j,k);
+    				for (int idx=0; idx<ReadValues.size(); idx++){
+    					signed char oldvalue=ReadValues[idx];
+    					signed char newvalue=WriteValues[idx];
+    					if (locval == oldvalue){
+    						data(i,j,k) = newvalue;
+    						idx = ReadValues.size();
+    					}
+    				}
+    			}
+    		}
+    	}
     }
-    
-    // Relabel the data
-	for (int k = 0; k<1024; k++){
-		for (int j = 0; j<1024; j++){
-			for (int i = 0; i<1024; i++){
-				//n = k*Nfx*Nfy + j*Nfx + i;
-				//char locval = loc_id[n];
-				char locval = data(i,j,k);
-				for (int idx=0; idx<ReadValues.size(); idx++){
-					signed char oldvalue=ReadValues[idx];
-					signed char newvalue=WriteValues[idx];
-					if (locval == oldvalue){
-						data(i,j,k) = newvalue;
-						idx = ReadValues.size();
-					}
-				}
-			}
-		}
-	}
 
     // Redistribute the data
     data = redistribute( srcRankInfo, data, rankInfo, { n[0], n[1], n[2] }, comm );
