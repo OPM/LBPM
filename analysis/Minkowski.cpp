dd4903155213e5ccc119dda1268ea29eaec3b9fe
JamesEMcclure <jemcclur@gmail.com>
2020-07-22 20:31:09 -0400

    fix smooth distance

--- analysis/Minkowski.cpp
+++ analysis/Minkowski.cpp
@@ -140,7 +140,7 @@ void Minkowski::MeasureObject(){
 	 *    0 - labels the object
 	 *    1 - labels the rest of the 
 	 */
-  DoubleArray smooth_distance(Nx,Ny,Nz);
+        //DoubleArray smooth_distance(Nx,Ny,Nz);
 	for (int k=0; k<Nz; k++){
 		for (int j=0; j<Ny; j++){
 			for (int i=0; i<Nx; i++){
@@ -149,8 +149,8 @@ void Minkowski::MeasureObject(){
 		}
 	}	
 	CalcDist(distance,id,*Dm);
-	Mean3D(distance,smooth_distance);
-	//Eikonal(distance, id, *Dm, 10, {true, true, true});
+	//Mean3D(distance,smooth_distance);
+	Eikonal(distance, id, *Dm, 20, {true, true, true});
 	ComputeScalar(smooth_distance,0.0);
 
 }
