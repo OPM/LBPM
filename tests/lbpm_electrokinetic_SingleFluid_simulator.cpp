31234ed0032da2b7275be4e564c4ba83dd66e44a
James McClure <mcclurej@vt.edu>
2021-01-15 14:41:10 -0500

    fix multiple init

--- tests/lbpm_electrokinetic_SingleFluid_simulator.cpp
+++ tests/lbpm_electrokinetic_SingleFluid_simulator.cpp
@@ -23,14 +23,12 @@ using namespace std;
 int main(int argc, char **argv)
 {
     // Initialize MPI and error handlers
-    Utilities::startup( argc, argv );
-
-    { // Limit scope so variables that contain communicators will free before MPI_Finialize
-    	// Initialize MPI
     	Utilities::startup( argc, argv );
     	Utilities::MPI comm( MPI_COMM_WORLD );
     	int rank = comm.getRank();
     	int nprocs = comm.getSize();
+
+    { // Limit scope so variables that contain communicators will free before MPI_Finialize
 
         if (rank == 0){
             printf("********************************************************\n");
