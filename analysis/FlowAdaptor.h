393fdaf5467ba71c3c40b2582ed37582f58b692e
James McClure <jemcclur@gmail.com>
2021-09-18 12:16:42 -0400

    update doxygen

--- analysis/FlowAdaptor.h
+++ analysis/FlowAdaptor.h
@@ -12,12 +12,48 @@
 
 #include "models/ColorModel.h"
 
+
+/**
+ * \class FlowAdaptor
+ *
+ * @brief 
+ * The FlowAdaptor class operates on a lattice Boltzmann model to alter the flow conditions
+ * 
+ */
+
 class FlowAdaptor{
 public:
+	
+
+    /**
+    * \brief Create a flow adaptor to operate on the LB model
+    * @param M        ScaLBL_ColorModel 
+    */
 	FlowAdaptor(ScaLBL_ColorModel &M);
+	
+    /**
+	* \brief Destructor
+	*/
 	~FlowAdaptor();
+	
+    /**
+    * \brief   Fast-forward interface motion
+    * \details Accelerate the movement of interfaces based on the time derivative
+    *       Optional keys to control behavior can be specified in the input database:
+    *          move_interface_cutoff  -- identifies the diffuse interface region
+    *          move_interface_factor -- determines how much to ``fast forward" 
+    * @param M        ScaLBL_ColorModel 
+    */
 	double MoveInterface(ScaLBL_ColorModel &M);
+	
+    /**
+     * \brief image re-initialization
+     * \details Re-initialize LB simulation from image data  
+     * @param M        ScaLBL_ColorModel 
+     * @param Filename    name of input file to be used to read image 
+    */
 	double ImageInit(ScaLBL_ColorModel &M, std::string Filename);
+	
 	double ShellAggregation(ScaLBL_ColorModel &M, const double delta_volume);
 	double UpdateFractionalFlow(ScaLBL_ColorModel &M);
 	double SeedPhaseField(ScaLBL_ColorModel &M, const double seed_water_in_oil);
--- doxygen/DoxygenMainpage.h
+++ doxygen/DoxygenMainpage.h
@@ -3,7 +3,12 @@
  * This is the documentation for LBPM
  *
  *   - \ref IO "IO routines"
+ *   - \ref analysis "Analysis routines"
+ *   - \ref FlowAdaptor "Flow adaptor"
+ *   - \ref models "Lattice Boltzmann models"
+ *   - \ref ScaLBL "Lattice Boltzmann building blocks"
+ *   - \ref tests "Unit tests"
  *   - \ref Utilities "Utility routines"
  *
- * \author James McClure
+ * \author J.E. McClure, M. Berrill
  */
