229feb93fb9d224780b7ba14f4042698eace37e6
James McClure <jemcclur@gmail.com>
2021-09-18 16:35:51 -0400

    extend doxygen support

--- doxygen/DoxygenMainpage.h
+++ doxygen/DoxygenMainpage.h
@@ -9,9 +9,6 @@
  *       - \ref Minkowski "Minkowski functionals"
  *   - \ref models "Lattice Boltzmann models"
  *   - \ref ScaLBL "Scalable Lattice Boltzmann Library (ScaLBL)"
- *   - \ref cpu "cpu implementation for library routines"
- *   - \ref cuda "CUDA implementation for library routines"
- *   - \ref hip "HIP implementation for library routines"
  *   - \ref tests "Unit tests"
  *   - \ref Utilities "Utility routines"
  *
