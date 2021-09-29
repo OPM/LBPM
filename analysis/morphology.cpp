b82a488f9124649da3f9dc2f44f0aa874f8ecb13
James McClure <jemcclur@gmail.com>
2021-09-29 12:48:31 -0400

    develop better interface...

--- analysis/morphology.cpp
+++ analysis/morphology.cpp
@@ -1,6 +1,119 @@
 #include <analysis/morphology.h>
 // Implementation of morphological opening routine
 
+
+Morphology::Morphology(std::shared_ptr <Domain> Dm){
+	int nx = Dm->Nx;
+	int ny = Dm->Ny;
+	int nz = Dm->Nz;
+	int nprocx = Dm->nprocx();
+	int nprocy = Dm->nprocy();
+	int nprocz = Dm->nprocz();
+	int rank = Dm->rank();
+	
+	// send buffers
+	sendID_x = new signed char [Dm->sendCount("x")];
+	sendID_y = new signed char [Dm->sendCount("y")];
+	sendID_z = new signed char [Dm->sendCount("z")];
+	sendID_X = new signed char [Dm->sendCount("X")];
+	sendID_Y = new signed char [Dm->sendCount("Y")];
+	sendID_Z = new signed char [Dm->sendCount("Z")];
+	sendID_xy = new signed char [Dm->sendCount("xy")];
+	sendID_yz = new signed char [Dm->sendCount("yz")];
+	sendID_xz = new signed char [Dm->sendCount("xz")];
+	sendID_Xy = new signed char [Dm->sendCount("Xy")];
+	sendID_Yz = new signed char [Dm->sendCount("Yz")];
+	sendID_xZ = new signed char [Dm->sendCount("xZ")];
+	sendID_xY = new signed char [Dm->sendCount("xY")];
+	sendID_yZ = new signed char [Dm->sendCount("yZ")];
+	sendID_Xz = new signed char [Dm->sendCount("Xz")];
+	sendID_XY = new signed char [Dm->sendCount("XY")];
+	sendID_YZ = new signed char [Dm->sendCount("YZ")];
+	sendID_XZ = new signed char [Dm->sendCount("XZ")];
+	//......................................................................................
+	// recv buffers
+	recvID_x = new signed char [Dm->recvCount("x")];
+	recvID_y = new signed char [Dm->recvCount("y")];
+	recvID_z = new signed char [Dm->recvCount("z")];
+	recvID_X = new signed char [Dm->recvCount("X")];
+	recvID_Y = new signed char [Dm->recvCount("Y")];
+	recvID_Z = new signed char [Dm->recvCount("Z")];
+	recvID_xy = new signed char [Dm->recvCount("xy")];
+	recvID_yz = new signed char [Dm->recvCount("yz")];
+	recvID_xz = new signed char [Dm->recvCount("xz")];
+	recvID_Xy = new signed char [Dm->recvCount("Xy")];
+	recvID_xZ = new signed char [Dm->recvCount("xZ")];
+	recvID_xY = new signed char [Dm->recvCount("xY")];
+	recvID_yZ = new signed char [Dm->recvCount("yZ")];
+	recvID_Yz = new signed char [Dm->recvCount("Yz")];
+	recvID_Xz = new signed char [Dm->recvCount("Xz")];
+	recvID_XY = new signed char [Dm->recvCount("XY")];
+	recvID_YZ = new signed char [Dm->recvCount("YZ")];
+	recvID_XZ = new signed char [Dm->recvCount("XZ")];
+}
+
+inline void Morphology::SendRecv(std::shared_ptr <Domain> Dm, char *id){
+	// Pack and send the updated ID values
+	PackID(Dm->sendList("x"), Dm->sendCount("x") ,sendID_x, id);
+	PackID(Dm->sendList("X"), Dm->sendCount("X") ,sendID_X, id);
+	PackID(Dm->sendList("y"), Dm->sendCount("y") ,sendID_y, id);
+	PackID(Dm->sendList("Y"), Dm->sendCount("Y") ,sendID_Y, id);
+	PackID(Dm->sendList("z"), Dm->sendCount("z") ,sendID_z, id);
+	PackID(Dm->sendList("Z"), Dm->sendCount("Z") ,sendID_Z, id);
+	PackID(Dm->sendList("xy"), Dm->sendCount("xy") ,sendID_xy, id);
+	PackID(Dm->sendList("Xy"), Dm->sendCount("Xy") ,sendID_Xy, id);
+	PackID(Dm->sendList("xY"), Dm->sendCount("xY") ,sendID_xY, id);
+	PackID(Dm->sendList("XY"), Dm->sendCount("XY") ,sendID_XY, id);
+	PackID(Dm->sendList("xz"), Dm->sendCount("xz") ,sendID_xz, id);
+	PackID(Dm->sendList("Xz"), Dm->sendCount("Xz") ,sendID_Xz, id);
+	PackID(Dm->sendList("xZ"), Dm->sendCount("xZ") ,sendID_xZ, id);
+	PackID(Dm->sendList("XZ"), Dm->sendCount("XZ") ,sendID_XZ, id);
+	PackID(Dm->sendList("yz"), Dm->sendCount("yz") ,sendID_yz, id);
+	PackID(Dm->sendList("Yz"), Dm->sendCount("Yz") ,sendID_Yz, id);
+	PackID(Dm->sendList("yZ"), Dm->sendCount("yZ") ,sendID_yZ, id);
+	PackID(Dm->sendList("YZ"), Dm->sendCount("YZ") ,sendID_YZ, id);
+	//......................................................................................
+	Dm->Comm.sendrecv(sendID_x,Dm->sendCount("x"),Dm->rank_x(),sendtag,recvID_X,Dm->recvCount("X"),Dm->rank_X(),recvtag);
+	Dm->Comm.sendrecv(sendID_X,Dm->sendCount("X"),Dm->rank_X(),sendtag,recvID_x,Dm->recvCount("x"),Dm->rank_x(),recvtag);
+	Dm->Comm.sendrecv(sendID_y,Dm->sendCount("y"),Dm->rank_y(),sendtag,recvID_Y,Dm->recvCount("Y"),Dm->rank_Y(),recvtag);
+	Dm->Comm.sendrecv(sendID_Y,Dm->sendCount("Y"),Dm->rank_Y(),sendtag,recvID_y,Dm->recvCount("y"),Dm->rank_y(),recvtag);
+	Dm->Comm.sendrecv(sendID_z,Dm->sendCount("z"),Dm->rank_z(),sendtag,recvID_Z,Dm->recvCount("Z"),Dm->rank_Z(),recvtag);
+	Dm->Comm.sendrecv(sendID_Z,Dm->sendCount("Z"),Dm->rank_Z(),sendtag,recvID_z,Dm->recvCount("z"),Dm->rank_z(),recvtag);
+	Dm->Comm.sendrecv(sendID_xy,Dm->sendCount("xy"),Dm->rank_xy(),sendtag,recvID_XY,Dm->recvCount("XY"),Dm->rank_XY(),recvtag);
+	Dm->Comm.sendrecv(sendID_XY,Dm->sendCount("XY"),Dm->rank_XY(),sendtag,recvID_xy,Dm->recvCount("xy"),Dm->rank_xy(),recvtag);
+	Dm->Comm.sendrecv(sendID_Xy,Dm->sendCount("Xy"),Dm->rank_Xy(),sendtag,recvID_xY,Dm->recvCount("xY"),Dm->rank_xY(),recvtag);
+	Dm->Comm.sendrecv(sendID_xY,Dm->sendCount("xY"),Dm->rank_xY(),sendtag,recvID_Xy,Dm->recvCount("Xy"),Dm->rank_Xy(),recvtag);
+	Dm->Comm.sendrecv(sendID_xz,Dm->sendCount("xz"),Dm->rank_xz(),sendtag,recvID_XZ,Dm->recvCount("XZ"),Dm->rank_XZ(),recvtag);
+	Dm->Comm.sendrecv(sendID_XZ,Dm->sendCount("XZ"),Dm->rank_XZ(),sendtag,recvID_xz,Dm->recvCount("xz"),Dm->rank_xz(),recvtag);
+	Dm->Comm.sendrecv(sendID_Xz,Dm->sendCount("Xz"),Dm->rank_Xz(),sendtag,recvID_xZ,Dm->recvCount("xZ"),Dm->rank_xZ(),recvtag);
+	Dm->Comm.sendrecv(sendID_xZ,Dm->sendCount("xZ"),Dm->rank_xZ(),sendtag,recvID_Xz,Dm->recvCount("Xz"),Dm->rank_Xz(),recvtag);
+	Dm->Comm.sendrecv(sendID_yz,Dm->sendCount("yz"),Dm->rank_yz(),sendtag,recvID_YZ,Dm->recvCount("YZ"),Dm->rank_YZ(),recvtag);
+	Dm->Comm.sendrecv(sendID_YZ,Dm->sendCount("YZ"),Dm->rank_YZ(),sendtag,recvID_yz,Dm->recvCount("yz"),Dm->rank_yz(),recvtag);
+	Dm->Comm.sendrecv(sendID_Yz,Dm->sendCount("Yz"),Dm->rank_Yz(),sendtag,recvID_yZ,Dm->recvCount("yZ"),Dm->rank_yZ(),recvtag);
+	Dm->Comm.sendrecv(sendID_yZ,Dm->sendCount("yZ"),Dm->rank_yZ(),sendtag,recvID_Yz,Dm->recvCount("Yz"),Dm->rank_Yz(),recvtag);
+	//......................................................................................
+	UnpackID(Dm->recvList("x"), Dm->recvCount("x") ,recvID_x, id);
+	UnpackID(Dm->recvList("X"), Dm->recvCount("X") ,recvID_X, id);
+	UnpackID(Dm->recvList("y"), Dm->recvCount("y") ,recvID_y, id);
+	UnpackID(Dm->recvList("Y"), Dm->recvCount("Y") ,recvID_Y, id);
+	UnpackID(Dm->recvList("z"), Dm->recvCount("z") ,recvID_z, id);
+	UnpackID(Dm->recvList("Z"), Dm->recvCount("Z") ,recvID_Z, id);
+	UnpackID(Dm->recvList("xy"), Dm->recvCount("xy") ,recvID_xy, id);
+	UnpackID(Dm->recvList("Xy"), Dm->recvCount("Xy") ,recvID_Xy, id);
+	UnpackID(Dm->recvList("xY"), Dm->recvCount("xY") ,recvID_xY, id);
+	UnpackID(Dm->recvList("XY"), Dm->recvCount("XY") ,recvID_XY, id);
+	UnpackID(Dm->recvList("xz"), Dm->recvCount("xz") ,recvID_xz, id);
+	UnpackID(Dm->recvList("Xz"), Dm->recvCount("Xz") ,recvID_Xz, id);
+	UnpackID(Dm->recvList("xZ"), Dm->recvCount("xZ") ,recvID_xZ, id);
+	UnpackID(Dm->recvList("XZ"), Dm->recvCount("XZ") ,recvID_XZ, id);
+	UnpackID(Dm->recvList("yz"), Dm->recvCount("yz") ,recvID_yz, id);
+	UnpackID(Dm->recvList("Yz"), Dm->recvCount("Yz") ,recvID_Yz, id);
+	UnpackID(Dm->recvList("yZ"), Dm->recvCount("yZ") ,recvID_yZ, id);
+	UnpackID(Dm->recvList("YZ"), Dm->recvCount("YZ") ,recvID_YZ, id);
+	//......................................................................................
+	
+}
+
 inline void PackID(const int *list, int count, signed char *sendbuf, signed char *ID){
 	// Fill in the phase ID values from neighboring processors
 	// This packs up the values that need to be sent from one processor to another
--- analysis/morphology.h
+++ analysis/morphology.h
@@ -6,3 +6,60 @@
 double MorphOpen(DoubleArray &SignDist, signed char *id, std::shared_ptr<Domain> Dm, double VoidFraction, signed char ErodeLabel, signed char ReplaceLabel);
 double MorphDrain(DoubleArray &SignDist, signed char *id, std::shared_ptr<Domain> Dm, double VoidFraction);
 double MorphGrow(DoubleArray &BoundaryDist, DoubleArray &Dist, Array<char> &id, std::shared_ptr<Domain> Dm, double TargetVol, double WallFactor);
+
+#ifndef MORPHOLOGY_INC
+#define MORPHOLOGY_INC
+/**
+ * \class Morphology
+ * @brief 
+ * The Morphology class supports morphological operations on complex structures
+ * 
+ */
+class Morphology{
+public:    
+	/**
+	* \brief Create a flow adaptor to operate on the LB model
+	* @param Dm       Domain structure 
+	*/
+	Morphology(std::shared_ptr <Domain> Dm);
+
+	/**
+	* \brief Destructor
+	*/
+	~Morphology();
+	
+	
+	/**
+	* \brief Send /recieve function for labels
+	*/
+	void SendRecv(std::shared_ptr <Domain> Dm, char *id);
+	
+	/**
+	* \brief  Find all sites such that the reach of the signed distance at the site  overlaps with a sub-domain boundary
+	*/
+	void GetOverlaps(DoubleArray &SignDist);
+
+
+private:
+	IntArray LocalOverlaps;
+	IntArray NonLocalOverlaps; 
+	
+	// List of sites that overlap with processor boundaries
+	int *sendOverlap_x, *sendOverlap_y, *sendOverlap_z, *sendOverlap_X, *sendOverlap_Y, *sendOverlap_Z;
+	int *sendOverlap_xy, *sendOverlap_yz, *sendOverlap_xz, *sendOverlap_Xy, *sendOverlap_Yz, *sendOverlap_xZ;
+	int *sendOverlap_xY, *sendOverlap_yZ, *sendOverlap_Xz, *sendOverlap_XY, *sendOverlap_YZ, *sendOverlap_XZ;
+	
+	int *recvOverlap_x, *recvOverlap_y, *recvOverlap_z, *recvOverlap_X, *recvOverlap_Y, *recvOverlap_Z;
+	int *recvOverlap_xy, *recvOverlap_yz, *recvOverlap_xz, *recvOverlap_Xy, *recvOverlap_Yz, *recvOverlap_xZ;
+	int *recvOverlap_xY, *recvOverlap_yZ, *recvOverlap_Xz, *recvOverlap_XY, *recvOverlap_YZ, *recvOverlap_XZ;
+	
+	// Communication buffers
+	signed char *sendID_x, *sendID_y, *sendID_z, *sendID_X, *sendID_Y, *sendID_Z;
+	signed char *sendID_xy, *sendID_yz, *sendID_xz, *sendID_Xy, *sendID_Yz, *sendID_xZ;
+	signed char *sendID_xY, *sendID_yZ, *sendID_Xz, *sendID_XY, *sendID_YZ, *sendID_XZ;
+	signed char *recvID_x, *recvID_y, *recvID_z, *recvID_X, *recvID_Y, *recvID_Z;
+	signed char *recvID_xy, *recvID_yz, *recvID_xz, *recvID_Xy, *recvID_Yz, *recvID_xZ;
+	signed char *recvID_xY, *recvID_yZ, *recvID_Xz, *recvID_XY, *recvID_YZ, *recvID_XZ;
+};
+
+#endif
