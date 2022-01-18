ee527a42cca9f216207a111f4a27d0a733feb8c9
Rex Zhe Li <zhe.rex.li@gmail.com>
2022-01-18 16:46:09 +1100

    update Poisson and Stoke solvers to include slipping velocity BC; to be built

--- models/PoissonSolver.h
+++ models/PoissonSolver.h
@@ -34,7 +34,7 @@ public:
     void ReadInput();
     void Create();
     void Initialize(double time_conv_from_Study);
-    void Run(double *ChargeDensity, int timestep_from_Study);
+    void Run(double *ChargeDensity, bool UseSlippingVelBC, int timestep_from_Study);
     void getElectricPotential(DoubleArray &ReturnValues);
     void getElectricPotential_debug(int timestep);
     void getElectricField(DoubleArray &Values_x, DoubleArray &Values_y,
