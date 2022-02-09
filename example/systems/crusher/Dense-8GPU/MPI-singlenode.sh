3c14c0cb89284b92f6400272a9eed7bbed391772
James E McClure <mcclurej@vt.edu>
2022-02-09 12:41:10 -0500

    fix binding in example

--- example/systems/crusher/Dense-8GPU/MPI-singlenode.sh
+++ example/systems/crusher/Dense-8GPU/MPI-singlenode.sh
@@ -28,7 +28,7 @@ export LBPM_BIN=/ccs/proj/csc380/mcclurej/crusher/LBPM/tests
 
 echo "Running Color LBM"
 
-MYCPUBIND="--cpu-bind=verbose,map_cpu:57"
+MYCPUBIND="--cpu-bind=verbose,map_cpu:57,33,25,1,9,17,41,49"
 
 srun --verbose -N1 -n8 --cpus-per-gpu=8 --gpus-per-task=1 --gpu-bind=closest ${MYCPUBIND} $LBPM_BIN/TestCommD3Q19 multinode.db
 #srun --verbose -N1 -n2 --mem-per-gpu=8g --cpus-per-gpu=1 --gpus-per-node=2 --gpu-bind=closest $LBPM_BIN/lbpm_permeability_simulator input.db
