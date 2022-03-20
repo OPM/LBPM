9e3a07d4192d2fa3184de2c6325e445937798ea2
James McClure <jemcclur@gmail.com>
2022-03-20 09:30:46 -0400

    add python script to generate bubble

--- example/Bubble/CreateBubble.py
+++ example/Bubble/CreateBubble.py
@@ -11,7 +11,7 @@ for i in range(0, 40):
     for j in range (0, 40):
         for k in range (0,40):
             dist = np.sqrt((i-cx)*(i-cx) + (j-cx)*(j-cx) + (k-cz)*(k-cz))
-            if (dist < 12.5 :
+            if (dist < 12.5 ) :
                 D[i,j,k] = 2
                 
 D.tofile("bubble_40x40x40.raw")
