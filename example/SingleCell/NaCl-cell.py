8fd8799ff9c7bebe10d21909aaaf396af844dd8f
James McClure <jemcclur@gmail.com>
2022-04-12 20:51:13 -0400

    update cell example

--- example/SingleCell/NaCl-cell.py
+++ example/SingleCell/NaCl-cell.py
@@ -1,37 +1,41 @@
 import numpy as np
 import matplotlib.pylab as plt
 
-D=np.ones((40,40,40),dtype="uint8")
+Nx = 64
+Ny = 64
+Nz = 64
+cx = Nx/2
+cy = Ny/2
+cz = Nz/2
+radius = 12
 
-cx = 20
-cy = 20
-cz = 20
+D=np.ones((Nx,Ny,Nz),dtype="uint8")
 
-for i in range(0, 40):
-    for j in range (0, 40):
-        for k in range (0,40):
+for i in range(0, Nx):
+    for j in range (0, Ny):
+        for k in range (0,Nz):
             dist = np.sqrt((i-cx)*(i-cx) + (j-cx)*(j-cx) + (k-cz)*(k-cz))
-            if (dist < 15.5 ) :
+            if (dist < radius ) :
                 D[i,j,k] = 2
 
-D.tofile("cell_40x40x40.raw")
+D.tofile("cell_64x64x64.raw")
 
                 
-C1=np.zeros((40,40,40),dtype="double")
-C2=np.zeros((40,40,40),dtype="double")
+C1=np.zeros((Nx,Ny,Nz),dtype="double")
+C2=np.zeros((Nx,Ny,Nz),dtype="double")
 
-for i in range(0, 40):
-    for j in range (0, 40):
-        for k in range (0,40):
+for i in range(0, Nx):
+    for j in range (0, Ny):
+        for k in range (0,Nz):
             #outside the cell
             C1[i,j,k] = 125.0e-6  # Na
             C2[i,j,k] = 125.0e-6  # Cl
             dist = np.sqrt((i-cx)*(i-cx) + (j-cx)*(j-cx) + (k-cz)*(k-cz))
             # inside the cell
-            if (dist < 15.5 ) :
-                C1[i,j,k] = 5.0e-6
-                C2[i,j,k] = 5.0e-6
+            if (dist < radius ) :
+                C1[i,j,k] = 110.0e-6
+                C2[i,j,k] = 110.0e-6
 
-C1.tofile("cell_concentration_Na_40x40x40.raw")
-C2.tofile("cell_concentration_Cl_40x40x40.raw")
+C1.tofile("cell_concentration_Na_64x64x64.raw")
+C2.tofile("cell_concentration_Cl_64x64x64.raw")
 
