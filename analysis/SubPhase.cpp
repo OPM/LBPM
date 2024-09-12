8f70e289410bf18cfc18eaac238f76ff6bd16ef9
Ricardoleite <ricardo_leitemartins@hotmail.com>
2024-09-12 15:38:16 -0300

    Correction: flow magnitude calculation

--- analysis/SubPhase.cpp
+++ analysis/SubPhase.cpp
@@ -402,7 +402,7 @@ void SubPhase::Basic() {
         double dir_x = gwb.Px + gnb.Px;
         double dir_y = gwb.Py + gnb.Py;
         double dir_z = gwb.Pz + gnb.Pz;
-        double flow_magnitude = dir_x * dir_x + dir_y * dir_y + dir_z * dir_z;
+        double flow_magnitude = sqrt(dir_x * dir_x + dir_y * dir_y + dir_z * dir_z);
         double force_mag = sqrt(Fx * Fx + Fy * Fy + Fz * Fz);
         if (force_mag > 0.0) {
             dir_x = Fx / force_mag;
